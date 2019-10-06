72765a04b63c0e02e5af7d93ad470b9d4b57f287
giancarlos <gianaugusto@gmail.com>
2019-10-06 22:37:00 +0100

    Spike localVPN from android

---
+++ app/src/main/java/com/macedo/bear/MainActivity.java
@@ -0,0 +1,63 @@
+package com.macedo.bear;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.VpnService;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.macedo.bear.Services.VPN.LocalVPNService;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int VPN_REQUEST_CODE = 0x0F;
+
+    private boolean waitingForVPNStart;
+
+    private BroadcastReceiver vpnStateReceiver = new BroadcastReceiver()
+    {
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            if (LocalVPNService.BROADCAST_VPN_STATE.equals(intent.getAction()))
+            {
+                if (intent.getBooleanExtra("running", false))
+                    waitingForVPNStart = false;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button vpnButton = findViewById(R.id.mainActivity_startMonitoring);
+        vpnButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startVPN();
+            }
+        });
+        waitingForVPNStart = false;
+        LocalBroadcastManager.getInstance(this).registerReceiver(vpnStateReceiver,
+                new IntentFilter(LocalVPNService.BROADCAST_VPN_STATE));
+    }
+
+    private void startVPN()
+    {
+        Intent vpnIntent = VpnService.prepare(this);
+        if (vpnIntent != null)
+            startActivityForResult(vpnIntent, VPN_REQUEST_CODE);
+        else
+            onActivityResult(VPN_REQUEST_CODE, RESULT_OK, null);
+    }
+}
---
+++ app/src/main/java/com/macedo/bear/Services/SyncService.java
@@ -0,0 +1,16 @@
+package com.macedo.bear.Services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class SyncService extends Service {
+    public SyncService () {
+    }
+
+    @Override
+    public IBinder onBind (Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
